a39feab4de21d7b1cf93b55e8fd2b2930bb70a21
ankiiisharma <myselfankit.2003@gmail.com>
2023-03-24 22:25:19 +0530

    WATER EQUIREMENT

---
+++ waterrequirement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        
+        int ans=n*2;
+        printf("%d\n",ans);
+    }
+	return 0;
+}
+
