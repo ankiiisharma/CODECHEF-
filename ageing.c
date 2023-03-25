969b97c2855e61ee0e834c668e1abd3737259cb6
ankiiisharma <myselfankit.2003@gmail.com>
2023-03-25 21:55:06 +0530

    AGEING @CODECHEF

---
+++ ageing.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	int x;
+	scanf("%d",&x);
+	
+	int ans = x-10;
+	
+	printf("%d\n",ans);
+	    
+	}
+	
+	
+	return 0;
+}
