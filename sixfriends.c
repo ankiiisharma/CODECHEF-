0cff575e47b502d26c0d25d34fd6d624788d8e0b
ankiiisharma <myselfankit.2003@gmail.com>
2023-03-27 22:09:28 +0530

    SIX FRIENDS @CODECHEF

---
+++ sixfriends.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    
+	    int X,Y;
+	    
+	    int A=3*X;
+	    int B=2*Y;
+	    
+	    
+	    if(A>=B)
+	    {
+	        printf("%d\n",A);
+	    }
+	    else
+	    {
+	        printf("%d\n",B);
+	    }
+	}
+	
+	return 0;
+}
+
