5a82a78b817ebdf78fff8a89af240306279ed869
ankiiisharma <myselfankit.2003@gmail.com>
2023-03-31 22:19:31 +0530

    Battery Health

---
+++ Batteryhealth.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    
+	    if(x>80)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
