201edcbb3bbe83f054a8000e9654fc5f999489e8
ankiiisharma <myselfankit.2003@gmail.com>
2023-04-08 22:42:08 +0530

    Better Deals Updated!

--- betterdeal.c
+++ betterdeal.c
@@ -8,8 +8,8 @@ int main(void) {
 	    int a,b;
 	    scanf("%d%d",&a,&b);
 	    
-	    int x=100-((a/100)*100);
-	    int y=200-((b/100)*200);
+	    int x=100-a;
+	    int y=200-(b*2);
 	    
 	    if(x<y)
 	    {
