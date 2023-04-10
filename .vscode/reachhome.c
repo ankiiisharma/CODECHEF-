556a67acf5c90c94e05b4fa785b48f7e3dad42df
ankiiisharma <myselfankit.2003@gmail.com>
2023-04-10 21:28:32 +0530

    REACH HOME @codechef

---
+++ .vscode/reachhome.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    
+	    int z=x*5:
+	    
+	    if(z>=y){
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
