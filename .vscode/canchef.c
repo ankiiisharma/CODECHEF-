39fb922a1e539127e210615b190c8c539d76c229
ankiiisharma <myselfankit.2003@gmail.com>
2023-04-14 23:39:45 +0530

    Can Chef

---
+++ .vscode/canchef.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x,y;
+	    scanf("%d%d",&x,&y);
+	    
+	    int ans=15*x;
+	    int z=2*y;
+	    
+	    if(z<=ans)
+	    {
+	        printf("YES\n");
+	    }
+	    else
+	    {
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
