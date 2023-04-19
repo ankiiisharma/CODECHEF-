22c7c5cfa39203789461e9cf6f76d2081b335774
ankiiisharma <myselfankit.2003@gmail.com>
2023-04-19 22:06:13 +0530

    OS Linux Fork Functions

---
+++ ForkFunction01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void main()
+{
+    int n;
+
+    printf("\n this program is for fork() system call \n");
+    // fork();
+    Fork();
+
+    printf("\n Hello \n");
+}
