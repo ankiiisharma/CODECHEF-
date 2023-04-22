7c5008318d49fa42c382d077abe0d3898efd0008
ankiiisharma <myselfankit.2003@gmail.com>
2023-04-22 21:08:18 +0530

    lunch @ codechef

---
+++ lunch.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    int x;
+	    scanf("%d",&x);
+	    if(x>=1 && x<=4)
+	    {
+	    printf("YES\n");
+	    }
+	    else
+	    {
+	    printf("NO\n");
+	    }
+	}
+	return 0;
+}
