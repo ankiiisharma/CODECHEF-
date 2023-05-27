44c3f00801a16af26a5199b08e69a76bd9335016
Ankiiisharma <myselfankit.2003@gmail.com>
2023-05-27 21:36:19 +0530

    Lapindromes in codechef(not completed)

---
+++ lapindromes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        char s[1000];
+        scanf("%s",&s);
+        
+        n=strlen(s);
+        
+        for(int i=0;i<n;i++)
+        {
+            if(s[i])
+        }
+        
+        
+    }
+	
+	
+	
+	return 0;
+}
+
