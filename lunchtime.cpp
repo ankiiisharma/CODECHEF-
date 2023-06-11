b47a6f0da4533f31314c91e83f59cee4a558e031
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-11 22:01:03 +0530

    Lunchtime.cpp

---
+++ lunchtime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    if(x<=4 && x>=1)
+	    {
+	        cout<<"YES"<<end;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+	return 0;
+}
