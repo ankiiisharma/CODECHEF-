c1fce64de326395a0d404851238651bcc6061780
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-28 18:59:50 +0530

    Broke String in CPP

---
+++ brokenstring.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    string s;
+	    cin>>s;
+	    
+	    // int x=(n/2);
+	    string a=s.substr(0,n/2);
+	    string b=s.substr(n/2,n);
+	    
+	    if(a==b)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
