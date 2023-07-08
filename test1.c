13c6e7e94142ed8933d3ffbb0b22d5f6af99bd08
Ankiiisharma <myselfankit.2003@gmail.com>
2023-07-08 17:13:49 +0530

    08-07*2023

---
+++ test1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char s[5];
+    char a[5];
+    scanf("%s",&s);
+
+    for(int i=5;i=0;i--)
+    {
+        printf("%s",a[i]);
+    }
+}
