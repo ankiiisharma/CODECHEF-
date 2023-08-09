097828dba1a659814027a16cd680f9dd7447e85c
Ankiiisharma <myselfankit.2003@gmail.com>
2023-08-09 11:56:50 +0530

    card swipe

--- cardswipes.cpp
+++ cardswipes.cpp
@@ -6,7 +6,9 @@ int main() {
 	cin>>t;
 	while(t--)
 	{
-	    int n,count;
+	    int n;
+		int count=0;
+		int count1=0;
 	    cin>>n;
 	    
 	    int a[n];
@@ -15,14 +17,29 @@ int main() {
 	    cin>>a[i];
 	    }
 	    
-	    for(int i=0;i<n;i++)
-	    {
-	        int ans=a[i]%2;
-	        if(ans!=0)
-	        {
-	            count=count+1;
-	        }
-	    }
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(a[i]==a[j])
+				{
+					count1 = count1 + 1;
+				}
+			}
+
+			if(count1 % 2==0)
+			{
+				count=count+1;
+			}
+		}
+	    // for(int i=0;i<n;i++)
+	    // {
+	    //     int ans=a[i]%2;
+	    //     if(ans!=0)
+	    //     {
+	    //         count=count+1;
+	    //     }
+	    // }
 	    
 	    cout<<count<<endl;
 	}
