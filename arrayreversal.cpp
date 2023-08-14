4f6df036eb9408d47c448564cb020eec216124ff
Ankiiisharma <myselfankit.2003@gmail.com>
2023-08-14 20:07:26 +0530

    array reversal

--- arrayreversal.cpp
+++ arrayreversal.cpp
@@ -3,28 +3,29 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
+   int n,temp,i,j;
+   cin>>n;
 
-    int a[n];
-    for(int i=0;i<n;i++)
+   int a[n];
+   for(i=0;i<n;i++)
+   {
+    cin>>a[i];
+   }
+
+    for(i=0,j=n-1;i<n/2;i++,j--)
     {
-        cin>>a[i];
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+
+        // i++;
+        // j--;
     }
 
-    int l=0;
-    int m=n;
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
 
-        for(int i=0;i<n;i++)
-        {
-            if(l<=n)
-            {
-            swap(a[l+i],a[l-i]);
-            }
-        }
 
-        for(int i=0;i<n;i++)
-        {
-            cout<<a[i]<<endl;
-        }
 }
