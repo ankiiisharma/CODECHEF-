5780a966ece4a8db7606bb404d06041979c2ec4b
Ankiiisharma <myselfankit.2003@gmail.com>
2023-08-19 23:49:05 +0530

    kth smallest element

--- kthsmalest.cpp
+++ kthsmalest.cpp
@@ -3,37 +3,41 @@
 using namespace std;
 
 
-int kthsmallest(int a[], int l, int n, int k);
-{
-    priority_queue<int> maxHeap;
-    for(int i=l;i<n;i++)
-    {
-        maxHeap.push(a[i]);
-        if(maxHeap.size()>k)
+class Solution{
+    public:
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int>maxHeap;
+        for(int i=l;i<=r;i++)
         {
-            maxHeap.pop();
+            maxHeap.push(arr[i]);
+            if(maxHeap.size()>k)
+            {
+                maxHeap.pop();
+            }
         }
+        return maxHeap.top();
+        
+        
     }
-
-    return maxHeap.top();
-}
+};
 
 int main()
 {
-    int n;
-    cin>>n;
-
-    int a[n];
-    int l=0;
-    for(int i=l;i<n;i++)
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
     {
-        cin>>a[i];
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
     }
-
-    int k;
-    cin>>k;
-
-    kthsmallest(int a[], int l, int n, int k);
-
     return 0;
 }
