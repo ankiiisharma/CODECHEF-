f2ab8595d26c0121c903943c8d3d3e62d1e85a87
Ankiiisharma <myselfankit.2003@gmail.com>
2023-08-21 20:33:44 +0530

    shorting negative elemenets

---
+++ recurssionreversalstring.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class solution
+{
+    public:
+        string reverseWord(string str& n)
+        {
+            // for(int i=0;i<str.lenght();i++)
+            // {
+            //     swap(a[i],a[str.lenght()-i-1]);
+            // } 
+            int n = str.lenght();
+        }
+};
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+    string s;
+    cin>>s;
+    solution ob;
+    cout<<ob.reverseWord(s)<<endl;
+    cout<<n;
+}
+
+return 0;
+}
