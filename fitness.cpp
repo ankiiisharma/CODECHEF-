8f8ba851e58a77f2813a533248106ac965c80cfd
Ankiiisharma <myselfankit.2003@gmail.com>
2023-08-31 23:19:13 +0530

    few simple maths questions

---
+++ fitness.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    int ans=10*x;
+	    
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
--- shortnegative.cpp
+++ shortnegative.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<vector>
 #include<algorithm>
+
 using namespace std;
 void move(vector<int>& arr){
   sort(arr.begin(),arr.end());
