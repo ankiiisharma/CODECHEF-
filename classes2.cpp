a478cc74aa61b89635637bf195af6b9f7d5d9caf
Ankiiisharma <myselfankit.2003@gmail.com>
2023-09-08 22:42:22 +0530

    classes 2

---
+++ classes2.cpp
@@ -0,0 +1,15 @@
+class MyClass {     
+   public:           
+    void myMethod();   
+};
+
+
+void MyClass::myMethod() {
+  cout << "Hello World!";
+}
+
+int main() {
+  MyClass myObj;    
+  myObj.myMethod(); 
+  return 0;
+}
